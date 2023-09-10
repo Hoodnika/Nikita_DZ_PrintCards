06c3fdb975f10a495f3f1ca8650feaa191b6b1a6
hoodnika <rhoodnika@gmail.com>
2023-09-11 02:20:51 +0500

    the main body of the program

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print(something)
+from funcs.utils import sorted_operations, censored_sender, cencored_addressee, new_time_format
+
+count = 0
+while count < 5:
+    print(new_time_format(count),end=" ")
+    print(sorted_operations()[count]["description"])
+    print(censored_sender(count),end=" ")
+    print(cencored_addressee(count))
+    print(sorted_operations()[count]["operationAmount"]["amount"],end=" ")
+    print(sorted_operations()[count]["operationAmount"]["currency"]["name"],end="\n\n")
+    count += 1
