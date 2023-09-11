528715db7cc35f9760fce00df30173ba2de4ea20
hoodnika <rhoodnika@gmail.com>
2023-09-11 23:27:07 +0500

    edit main.py, after editing funcs

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from funcs.utils import sorted_operations, censored_sender, cencored_addressee, 
 
 count = 0
 while count < 5:
-    print(new_time_format(count),end=" ")
-    print(sorted_operations()[count]["description"])
-    print(censored_sender(count),end=" ")
-    print(cencored_addressee(count))
-    print(sorted_operations()[count]["operationAmount"]["amount"],end=" ")
-    print(sorted_operations()[count]["operationAmount"]["currency"]["name"],end="\n\n")
+    print(new_time_format("operations.json", count),end=" ")
+    print(sorted_operations("operations.json")[count]["description"])
+    print(censored_sender("operations.json", count),end=" ")
+    print(cencored_addressee("operations.json", count))
+    print(sorted_operations("operations.json")[count]["operationAmount"]["amount"],end=" ")
+    print(sorted_operations("operations.json")[count]["operationAmount"]["currency"]["name"],end="\n\n")
     count += 1
